e906764dc933b6da8a070c09f3d1c14acb92395c
codingisacopingstrategy <eric@ericschrijver.nl>
2011-02-18 14:30:10 +0100

    And a better Bowie

--- mt.js
+++ mt.js
@@ -100,7 +100,7 @@ function mtShowCaptcha() {
     if (mtCaptchaVisible) return;
     var div = document.getElementById('comments-open-captcha');
     if (div) {
-        div.innerHTML = '<div class="label"><label for="captcha_code">Captcha:</label></div><div class="field"><input type="hidden" name="token" value="8rHqzuUpZktw8jT0ErIdcd4oRKg8qAiDutbXhfQe" /><img src="http://mt.schr.fr/mt-comments.cgi/captcha/1/8rHqzuUpZktw8jT0ErIdcd4oRKg8qAiDutbXhfQe" width="150" height="35" /><br /><input name="captcha_code" id="captcha_code" value="" autocomplete="off" /><p>Type the characters you see in the picture above.</p></div>';
+        div.innerHTML = '<div class="label"><label for="captcha_code">Captcha:</label></div><div class="field"><input type="hidden" name="token" value="F4VvZOBY4i3eLICQOaoghggwjpg5BNqVVDbjo9Xc" /><img src="http://mt.schr.fr/mt-comments.cgi/captcha/1/F4VvZOBY4i3eLICQOaoghggwjpg5BNqVVDbjo9Xc" width="150" height="35" /><br /><input name="captcha_code" id="captcha_code" value="" autocomplete="off" /><p>Type the characters you see in the picture above.</p></div>';
         mtCaptchaVisible = true;
     }
 }
