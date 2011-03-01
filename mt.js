968e16bc5b3b530a68745fe9f1c4c25a7e189507
codingisacopingstrategy <eric@ericschrijver.nl>
2011-03-01 18:55:18 +0100

    Underscores to dashes
    Even though it breaks my heart.
    But this is supposed to be best for SEO.
    I feel like a trend follower now
    My

--- mt.js
+++ mt.js
@@ -100,7 +100,7 @@ function mtShowCaptcha() {
     if (mtCaptchaVisible) return;
     var div = document.getElementById('comments-open-captcha');
     if (div) {
-        div.innerHTML = '<div class="label"><label for="captcha_code">Captcha:</label></div><div class="field"><input type="hidden" name="token" value="laFeAzuxCpwiuXz8CHqgJ2r7otgnKcK2ESRt1HLV" /><img src="http://mt.schr.fr/mt-comments.cgi/captcha/1/laFeAzuxCpwiuXz8CHqgJ2r7otgnKcK2ESRt1HLV" width="150" height="35" /><br /><input name="captcha_code" id="captcha_code" value="" autocomplete="off" /><p>Type the characters you see in the picture above.</p></div>';
+        div.innerHTML = '<div class="label"><label for="captcha_code">Captcha:</label></div><div class="field"><input type="hidden" name="token" value="trQvigBmihoNYK3C4U200Z31XZPE4vopj1RQ1KNY" /><img src="http://mt.schr.fr/mt-comments.cgi/captcha/1/trQvigBmihoNYK3C4U200Z31XZPE4vopj1RQ1KNY" width="150" height="35" /><br /><input name="captcha_code" id="captcha_code" value="" autocomplete="off" /><p>Type the characters you see in the picture above.</p></div>';
         mtCaptchaVisible = true;
     }
 }
