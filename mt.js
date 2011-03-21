b5f9a785e28caad1c197d52bc540f926bae15da2
codingisacopingstrategy <eric@ericschrijver.nl>
2011-03-21 14:48:21 +0100

    Archives link in header

--- mt.js
+++ mt.js
@@ -100,7 +100,7 @@ function mtShowCaptcha() {
     if (mtCaptchaVisible) return;
     var div = document.getElementById('comments-open-captcha');
     if (div) {
-        div.innerHTML = '<div class="label"><label for="captcha_code">Captcha:</label></div><div class="field"><input type="hidden" name="token" value="trQvigBmihoNYK3C4U200Z31XZPE4vopj1RQ1KNY" /><img src="http://mt.schr.fr/mt-comments.cgi/captcha/1/trQvigBmihoNYK3C4U200Z31XZPE4vopj1RQ1KNY" width="150" height="35" /><br /><input name="captcha_code" id="captcha_code" value="" autocomplete="off" /><p>Type the characters you see in the picture above.</p></div>';
+        div.innerHTML = '<div class="label"><label for="captcha_code">Captcha:</label></div><div class="field"><input type="hidden" name="token" value="KmAmRoRCHTbfHTEDh548btSuKEYCfPsgioYEXVdz" /><img src="http://mt.schr.fr/mt-comments.cgi/captcha/1/KmAmRoRCHTbfHTEDh548btSuKEYCfPsgioYEXVdz" width="150" height="35" /><br /><input name="captcha_code" id="captcha_code" value="" autocomplete="off" /><p>Type the characters you see in the picture above.</p></div>';
         mtCaptchaVisible = true;
     }
 }
