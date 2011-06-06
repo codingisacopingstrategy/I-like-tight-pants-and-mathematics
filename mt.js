ff99b0028bb96b8143cf905f11151c65f9f60dfb
codingisacopingstrategy <eric@ericschrijver.nl>
2011-06-07 01:34:01 +0200

    Now a number of new authors

--- mt.js
+++ mt.js
@@ -100,7 +100,7 @@ function mtShowCaptcha() {
     if (mtCaptchaVisible) return;
     var div = document.getElementById('comments-open-captcha');
     if (div) {
-        div.innerHTML = '<div class="label"><label for="captcha_code">Captcha:</label></div><div class="field"><input type="hidden" name="token" value="g2N01kGcMm7exomald8bxksh8H1qBJkWvZ6mjb6k" /><img src="http://mt.schr.fr/comments.cgi/captcha/1/g2N01kGcMm7exomald8bxksh8H1qBJkWvZ6mjb6k" width="150" height="35" /><br /><input name="captcha_code" id="captcha_code" value="" autocomplete="off" /><p>Type the characters you see in the picture above.</p></div>';
+        div.innerHTML = '<div class="label"><label for="captcha_code">Captcha:</label></div><div class="field"><input type="hidden" name="token" value="q4eYVegYwpjBa0mqTERAypc703agQArGFHgmXsnh" /><img src="http://mt.schr.fr/comments.cgi/captcha/1/q4eYVegYwpjBa0mqTERAypc703agQArGFHgmXsnh" width="150" height="35" /><br /><input name="captcha_code" id="captcha_code" value="" autocomplete="off" /><p>Type the characters you see in the picture above.</p></div>';
         mtCaptchaVisible = true;
     }
 }
