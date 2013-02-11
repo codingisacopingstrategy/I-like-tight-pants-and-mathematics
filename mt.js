dddd360c0c5d7b6f907482514dce1c3bc3667fd1
Eric Schrijver <eric@ericschrijver.nl>
2013-02-12 00:17:52 +0100

    Repairing after an encoding issue.
    All ’é… etc were garbled.
    Had to reverse engineer what the garbled characters represented so there might be some changes.
    
    Also, removed commented out code.

--- mt.js
+++ mt.js
@@ -100,7 +100,7 @@ function mtShowCaptcha() {
     if (mtCaptchaVisible) return;
     var div = document.getElementById('comments-open-captcha');
     if (div) {
-        div.innerHTML = '<div class="label"><label for="captcha_code">Captcha:</label></div><div class="field"><input type="hidden" name="token" value="i7hC0102Up5UzUDYlZVCpLVKLMVTUGoTXUzytm22" /><img src="http://mt.schr.fr/comments.cgi/captcha/1/i7hC0102Up5UzUDYlZVCpLVKLMVTUGoTXUzytm22" width="150" height="35" /><br /><input name="captcha_code" id="captcha_code" value="" autocomplete="off" /><p>Type the characters you see in the picture above.</p></div>';
+        div.innerHTML = '<div class="label"><label for="captcha_code">Captcha:</label></div><div class="field"><input type="hidden" name="token" value="Y3LPoLSJStLaS7bRYu32MC7iSJ85SfO1RU6hCjGV" /><img src="http://mt.schr.fr/comments.cgi/captcha/1/Y3LPoLSJStLaS7bRYu32MC7iSJ85SfO1RU6hCjGV" width="150" height="35" /><br /><input name="captcha_code" id="captcha_code" value="" autocomplete="off" /><p>Type the characters you see in the picture above.</p></div>';
         mtCaptchaVisible = true;
     }
 }
