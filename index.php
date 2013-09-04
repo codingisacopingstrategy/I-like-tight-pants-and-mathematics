9ec3f29b8e0bd924d1ba6b68247eac0ffda77ea6
Eric Schrijver <eric@ericschrijver.nl>
2013-09-04 22:57:25 +0200

    New article The Story of Nokia, Microsoft and a 1000 Hearts Breaking

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-header('Location: /and/it-might-be-a-unix-sin-but-i-would-like-you-to-add-dot-t-x-t-as-the-extension-of-your-readme-file');
+header('Location: /and/the-story-of-nokia-microsoft-and-a-1000-hearts-breaking');
 ?>
--- mt.js
+++ mt.js
@@ -100,7 +100,7 @@ function mtShowCaptcha() {
     if (mtCaptchaVisible) return;
     var div = document.getElementById('comments-open-captcha');
     if (div) {
-        div.innerHTML = '<div class="label"><label for="captcha_code">Captcha:</label></div><div class="field"><input type="hidden" name="token" value="PWftCUFVggHts9EmwMQACnc5TXTUGsxyH9RcOBcb" /><img src="http://mt.schr.fr/comments.cgi/captcha/1/PWftCUFVggHts9EmwMQACnc5TXTUGsxyH9RcOBcb" width="150" height="35" /><br /><input name="captcha_code" id="captcha_code" value="" autocomplete="off" /><p>Type the characters you see in the picture above.</p></div>';
+        div.innerHTML = '<div class="label"><label for="captcha_code">Captcha:</label></div><div class="field"><input type="hidden" name="token" value="cn6Qb6rS7o8hIAuJkQUAGWyekzHPG0FnbZaLK6xN" /><img src="http://mt.schr.fr/comments.cgi/captcha/1/cn6Qb6rS7o8hIAuJkQUAGWyekzHPG0FnbZaLK6xN" width="150" height="35" /><br /><input name="captcha_code" id="captcha_code" value="" autocomplete="off" /><p>Type the characters you see in the picture above.</p></div>';
         mtCaptchaVisible = true;
     }
 }
