41c181a8ba9416a53c4de0071fa79316073c06a4
Eric Schrijver <eric@ericschrijver.nl>
2013-10-20 18:09:59 +0200

    Try new whitespace fix (problems with inline’s)

--- mt.js
+++ mt.js
@@ -100,7 +100,7 @@ function mtShowCaptcha() {
     if (mtCaptchaVisible) return;
     var div = document.getElementById('comments-open-captcha');
     if (div) {
-        div.innerHTML = '<div class="label"><label for="captcha_code">Captcha:</label></div><div class="field"><input type="hidden" name="token" value="tIQygE9LXreYaDuud9wsRNEZ0c9eYruX4tUecB6x" /><img src="http://mt.schr.fr/comments.cgi/captcha/1/tIQygE9LXreYaDuud9wsRNEZ0c9eYruX4tUecB6x" width="150" height="35" /><br /><input name="captcha_code" id="captcha_code" value="" autocomplete="off" /><p>Type the characters you see in the picture above.</p></div>';
+        div.innerHTML = '<div class="label"><label for="captcha_code">Captcha:</label></div><div class="field"><input type="hidden" name="token" value="1iYKzZuKVwZC0hJpMEYXDGiFlZkgXdF2K6aWki12" /><img src="http://mt.schr.fr/comments.cgi/captcha/1/1iYKzZuKVwZC0hJpMEYXDGiFlZkgXdF2K6aWki12" width="150" height="35" /><br /><input name="captcha_code" id="captcha_code" value="" autocomplete="off" /><p>Type the characters you see in the picture above.</p></div>';
         mtCaptchaVisible = true;
     }
 }
