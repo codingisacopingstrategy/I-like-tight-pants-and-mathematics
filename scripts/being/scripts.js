3df0ba7d2b682bcea610463ee5db8807172681a4
Eric Schrijver <eric@ericschrijver.nl>
2014-02-20 15:11:25 +0100

    Update sidebar script to allow overriding of style

--- scripts/being/scripts.js
+++ scripts/being/scripts.js
@@ -1,15 +1,11 @@
 $(function() {
   if ($("#aside").length !== 0) {
     $("div.article").removeClass("suffix_2");
-    $("#aside").attr("style", "padding-top: 44px;")
-    $("#aside").addClass("grid_2")
+    if (!$("#aside").is("[style]")) {
+        $("#aside").attr("style", "padding-top: 44px;");
+    }
+    $("#aside").addClass("grid_2");
     $("div.post").append($("#aside"));
-  }
-  if ($("#aside-left").length !== 0) {
-    $("div.article").removeClass("suffix_2");
-    $("#aside-left").attr("style", "padding-top: 44px;")
-    $("#aside-left").addClass("grid_2")
-    $("div.post").prepend($("#aside-left"));
   }
 });
 
