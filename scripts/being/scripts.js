4b6298af74dd8c6d715506a479fb406ae558109d
Eric Schrijver <eric@ericschrijver.nl>
2018-06-25 22:24:35 +0200

    Replace MT js for comments

--- scripts/being/scripts.js
+++ scripts/being/scripts.js
@@ -9,3 +9,39 @@ $(function() {
   }
 });
 
+
+function mtReplyCommentOnClick(parent_id, author) {
+    $('#comment-form-reply').show();
+
+    var checkbox = document.getElementById('comment-reply');
+    var label = document.getElementById('comment-reply-label');
+    var text = document.getElementById('comment-text');
+
+    // Populate label with new values
+    var reply_text = 'Replying to \<a href=\"#comment-__PARENT__\" onclick=\"location.href=this.href; return false\"\>comment from __AUTHOR__\<\/a\>';
+    reply_text = reply_text.replace(/__PARENT__/, parent_id);
+    reply_text = reply_text.replace(/__AUTHOR__/, author);
+    label.innerHTML = reply_text;
+
+    checkbox.value = parent_id; 
+    checkbox.checked = true;
+    try {
+        // text field may be hidden
+        text.focus();
+    } catch(e) {
+    }
+
+    mtSetCommentParentID();
+}
+
+
+function mtSetCommentParentID() {
+    var checkbox = document.getElementById('comment-reply');
+    var parent_id_field = document.getElementById('id_parent');
+    if (!checkbox || !parent_id_field) return;
+
+    var pid = null;
+    if (checkbox.checked == true)
+        pid = checkbox.value;
+    parent_id_field.value = pid;
+}
