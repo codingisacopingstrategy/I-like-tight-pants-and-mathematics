75d0d5e592b5a68f4ef7a58e9086abd69bed96c7
Eric Schrijver <eric@ericschrijver.nl>
2026-03-16 10:42:27 +0100

    Automatic reload for generating css from less (with only bash built-ins)

--- the/style/in/less.sh
+++ the/style/in/less.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-lessc style.less style.css
+
+FILE="style.less"
+LAST=""
+
+while true; do
+    NEW=$(stat -f %m "$FILE")
+    if [ "$NEW" != "$LAST" ]; then
+        LAST=$NEW
+        echo "RUNNING lessc style.less style.css"
+        lessc style.less style.css
+        echo "waiting for change…"
+    fi
+    sleep 1
+done
